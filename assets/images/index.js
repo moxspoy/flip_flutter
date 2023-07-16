deed069736225b441b02ed8892a140f2dc1b443b
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-16 22:25:24 +0700

    remove js file

--- assets/images/index.js
+++
@@ -1,9 +0,0 @@
-import Bank from './banks';
-import Wallet from './wallets';
-import Icon from './icons';
-import Illustration from './illustrations';
-import ProductIcon from './products';
-import Tutorial from './tutorials';
-import Loading from './loading';
-
-export {Bank, Icon, Illustration, ProductIcon, Tutorial, Wallet, Loading};
--- assets/images/wallets/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-    dana: require('./dana.png'),
-    gopay: require('./gopay.png'),
-    linkaja: require('./linkaja.png'),
-    ovo: require('./ovo.png'),
-    shopeepay: require('./shopeepay.png'),
-    qris: require('./qris.png'),
-};
