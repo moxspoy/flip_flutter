6675f501ca153bff8a634d334f9b639e85958f17
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-22 09:46:03 +0700

    create navigation route

---
+++ lib/constants/navigation.dart
@@ -0,0 +1,16 @@
+class NavigationRouteName {
+  static const splash = 'splash';
+  static const login = 'login';
+  static const register = 'register';
+  static const home = 'home';
+  static const pin = 'pin';
+  static const otp = 'otp';
+  static const transactionHistory = 'transactionHistory';
+  static const formSendMoney = 'formSendMoney';
+  static const formCredit = 'formCredit';
+  static const formElectricity = 'formElectricity';
+  static const payment = 'payment';
+  static const transactionProgress = 'transactionProgress';
+  static const profile = 'profile';
+  static const webView = 'webView';
+}
