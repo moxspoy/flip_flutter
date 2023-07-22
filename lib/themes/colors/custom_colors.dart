3119cdb4f0ed3fcc731abef897ce233edd1c0ba5
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-22 23:24:44 +0700

    create custom color

---
+++ lib/themes/colors/custom_colors.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color flipOrangePlus2 = Color(0xFF7E3221);
+  static const Color flipOrangePlus1 = Color(0xFFD35437);
+  static const Color flipOrange = Color(0xFFFD6542);
+  static const Color flipOrangeMin1 = Color(0xFFFE9881);
+  static const Color flipOrangeMin2 = Color(0xFFFEC1B3);
+  static const Color flipOrangeMin3 = Color(0xFFFDEAE6);
+  static const Color yellowPlus2 = Color(0xFF543D07);
+  static const Color yellowPlus1 = Color(0xFFA87A0D);
+  static const Color yellow = Color(0xFFFCB714);
+  static const Color yellowMin1 = Color(0xFFFDCF62);
+  static const Color yellowMin2 = Color(0xFFFEE7B1);
+  static const Color yellowMin3 = Color(0xFFFEF1D0);
+  static const Color pinkPlus2 = Color(0xFF8A1A38);
+  static const Color pinkPlus1 = Color(0xFFC61F4D);
+  static const Color pink = Color(0xFFEE255C);
+  static const Color pinkMin1 = Color(0xFFF46E92);
+  static const Color pinkMin2 = Color(0xFFF9B6C9);
+  static const Color pinkMin3 = Color(0xFFFCD8E1);
+  static const Color pinkMin4 = Color(0xFFF4FBFF);
+  static const Color crimsonRedPlus2 = Color(0xFF8A1A38);
+  static const Color crimsonRedPlus1 = Color(0xFFC61F4D);
+  static const Color crimsonRed = Color(0xFFEE255C);
+  static const Color crimsonRedMin1 = Color(0xFFF46E92);
+  static const Color crimsonRedMin2 = Color(0xFFF9B6C9);
+  static const Color crimsonRedMin3 = Color(0xFFFDE9EF);
+  static const Color greenPlus2 = Color(0xFF17403A);
+  static const Color greenPlus1 = Color(0xFF2F7F74);
+  static const Color green = Color(0xFF46BFAE);
+  static const Color greenMin1 = Color(0xFF84D4C9);
+  static const Color greenMin2 = Color(0xFFC1EAE4);
+  static const Color greenMin3 = Color(0xFFDAF2EF);
+  //Golden
+  static const Color goldenYellowMin1 = Color(0xFFFDCF62);
+  static const Color goldenYellowMin2 = Color(0xFFFEE7B1);
+  static const Color goldenYellowMin3 = Color(0xFFFDF6DC);
+  static const Color goldenYellowPlus2 = Color(0xFF543D07);
+  // Jade Green
+  static const Color jadeGreenPlus2 = Color(0xFF236057);
+  static const Color jadeGreenPlus1 = Color(0xFF2F7F74);
+  static const Color jadeGreen = Color(0xFF46BFAE);
+  static const Color jadeGreenMin1 = Color(0xFF84D4C9);
+  static const Color jadeGreenMin2 = Color(0xFFC1EAE4);
+  static const Color jadeGreenMin3 = Color(0xFFDAF2EF);
+  static const Color jadeLight = Color(0xFFE9FFF8);
+  // Purple
+  static const Color purplePlus2 = Color(0xFF3B0A2D);
+  static const Color purplePlus1 = Color(0xFF75135B);
+  static const Color purple = Color(0xFFB01D88);
+  static const Color purpleMin1 = Color(0xFFCA68B0);
+  static const Color purpleMin2 = Color(0xFFE5B4D7);
+  static const Color purpleMin3 = Color(0xFFEFD2E7);
+  // Blue
+  static const Color bluePlus2 = Color(0xFF223547);
+  static const Color bluePlus1 = Color(0xFF446A8F);
+  static const Color blue = Color(0xFF5786CC);
+  static const Color blueMin1 = Color(0xFF99BFE4);
+  static const Color blueMin2 = Color(0xFFCCDFF1);
+  static const Color blueMin3 = Color(0xFFE0ECF7);
+  static const Color aliceBlue = Color(0xFFF4FBFF);
+  static const Color blueSky = Color(0xFF009CFE);
+  // Sapphire Blue
+  static const Color sapphireBluePlus2 = Color(0xFF1D2D44);
+  static const Color sapphireBluePlus1 = Color(0xFF3A5988);
+  static const Color sapphireBlue = Color(0xFF5785CC);
+  static const Color sapphireBlueMin1 = Color(0xFF8FAEDD);
+  static const Color sapphireBlueMin2 = Color(0xFFC7D7EE);
+  static const Color sapphireBlueMin3 = Color(0xFFDDE7F5);
+  // Grey
+  static const Color greyPlus2 = Color(0xFF222223);
+  static const Color greyPlus1 = Color(0xFF555556);
+  static const Color grey = Color(0xFFAAABAD);
+  static const Color greyMin1 = Color(0xFFC6C7C8);
+  static const Color greyMin2 = Color(0xFFE3E3E4);
+  static const Color greyMin3 = Color(0xFFEEEEEF);
+  static const Color lightGrey = Color(0xFFDEDEDE);
+  static const Color darkGrey = Color(0xFFA9A9A9);
+  static const Color greyPlaceholder = Color(0xFFA4A4A4);
+  // Etc
+  static const Color blackBekko = Color(0xFF333333);
+  static const Color lightAnemia = Color(0xFFFFF5F3);
+  static const Color lightSmoke = Color(0xFFF9F9F9);
+  static const Color freshWater = Color(0xFFF3F7F9); //background
+  static const Color ketchupTomato = Color(0xFFD42E52);
+  static const Color transparent = Colors.transparent;
+  static const Color specialWidget = Color(0xFFEB451E);
+  // Neutral
+  static const Color mainText = Color(0xFF222223);
+  static const Color secondaryText = Color(0xFF747474);
+  static const Color borderDisabled = Color(0xFFE3E3E4);
+  static const Color containerSecond = Color(0xFFF3F5F6);
+  static const Color disabled = Color(0xFFE3E3E4);
+  static const Color container = Color(0xFFF3F5F6);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color customBlack30Percent = Color.fromRGBO(0, 0, 0, 0.3);
+  static const Color customBlack80Percent = Color.fromRGBO(0, 0, 0, 0.8);
+  static const Color customShadowGrey = Color(0xFFB5B5B5);
+  static const Color greyLight = Color(0xFFE5E5E5);
+  static const Color greyLight2 = Color(0xFFE2E2E2);
+  static const Color overlay70 = Color.fromRGBO(34, 34, 35, 0.7);
+  // New Green
+  static const Color newGreen = Color(0xFF00A68E);
+
+  static const Color shadow1 = Color.fromRGBO(255, 255, 255, 0);
+  static const Color shadow2 = Color.fromRGBO(255, 255, 255, 0.96875);
+
+}
