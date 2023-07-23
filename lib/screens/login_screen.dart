e7b046dcefb4311a6f3c3c4481e8fffa851cc0c2
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-23 09:49:35 +0700

    use custom app bar widget

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,6 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
-import 'package:circle_flags/circle_flags.dart';
-import 'package:flip/constants/l10n.dart';
 import 'package:flip/utils/l10n/localizations.dart';
-import 'package:flip/widgets/bottomsheet/change_language.dart';
+import 'package:flip/widgets/appbar/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:phone_form_field/phone_form_field.dart';
 
@@ -12,45 +10,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 90,
-          backgroundColor:
-              AdaptiveTheme.of(context).theme.primaryColor.withOpacity(0.8),
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 16),
-            child: Image.asset(
-              'assets/images/icons/flip.png',
-              height: 50,
-              width: 50,
-            ),
-          ),
-          actions: [
-            Center(
-              child: InkWell(
-                child: CircleFlag(
-                  Localizations.localeOf(context).languageCode ==
-                          LanguageProvider().languageIndonesia
-                      ? 'id'
-                      : 'gb',
-                  size: 24.0,
-                ),
-                onTap: () {
-                  ChangeLanguageBottomSheet.show(context);
-                },
-              ),
-            ),
-            IconButton(
-                onPressed: () {
-                  AdaptiveTheme.of(context).toggleThemeMode();
-                },
-                icon: Icon(AdaptiveTheme.of(context).mode ==
-                    AdaptiveThemeMode.dark
-                    ? Icons.light_mode
-                    : Icons.dark_mode
-                )
-            )
-          ],
-        ),
+        appBar: const OnboardingAppBar(),
         body: Container(
           margin: const EdgeInsets.only(left: 16, right: 16, top: 16),
           child: Column(
