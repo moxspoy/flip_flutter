790052f3b14bb061158398c84079e9b2b2eb0e21
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-24 22:18:33 +0700

    fix validation rules

--- lib/utils/validation/validation.dart
+++ lib/utils/validation/validation.dart
@@ -1,7 +1,9 @@
 String validateMobile(String value) {
+  value =
+      value.replaceAll(RegExp(r'[^0-9]'), '').replaceFirst(RegExp(r'62'), '0');
   String pattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
   RegExp regExp = RegExp(pattern);
-  if (value.isEmpty || value.length <= 11) {
+  if (value.isEmpty || value.length <= 9) {
     return 'Please enter mobile number';
   } else if (!regExp.hasMatch(value)) {
     return 'Please enter valid mobile number';
