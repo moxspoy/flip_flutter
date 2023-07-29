e264f286e150e743d5cae86570d9612f18d39469
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-29 11:06:40 +0700

    change headline text color

--- lib/themes/text/custom_text_style.dart
+++ lib/themes/text/custom_text_style.dart
@@ -1,3 +1,5 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flip/themes/colors/custom_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextStyle {
@@ -10,7 +12,10 @@ class CustomTextStyle {
   static TextStyle? headlineLarge(BuildContext context) {
     TextTheme existingTextTheme = Theme.of(context).textTheme;
 
-    return existingTextTheme.headlineLarge
-        ?.copyWith(fontWeight: FontWeight.w700);
+    return existingTextTheme.headlineLarge?.copyWith(
+        fontWeight: FontWeight.w700,
+        color: AdaptiveTheme.of(context).brightness == Brightness.light
+            ? CustomColor.blackBekko
+            : CustomColor.white);
   }
 }
