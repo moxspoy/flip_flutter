3898d14414a2b0051a4047ee6373e6c582b7eaf9
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-29 09:17:30 +0700

    button common widget

---
+++ lib/widgets/button/button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.onPressed,
+    required this.child,
+    this.isLoading = false,
+    this.isExpanded = false,
+    this.text = '',
+    this.customStyle,
+  });
+
+  final VoidCallback? onPressed;
+  final Widget? child;
+  final bool? isLoading;
+  final bool? isExpanded;
+  final ButtonStyle? customStyle;
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: customStyle ??
+          ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50)),
+      child: isLoading == true
+          ? const SizedBox(
+              height: 16,
+              width: 16,
+              child: Center(
+                  child: CircularProgressIndicator(
+                color: Colors.white,
+                strokeWidth: 1.5,
+              )))
+          : child ?? (text != '' ? Text(text!) : child),
+    );
+  }
+}
