cc6fb9a2f516fa79b07141a4636c17b00d5fd4c5
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-29 10:49:57 +0700

    rename navigation container to application container

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return NavigationContainer(
+    return ApplicationContainer(
       savedThemeMode: savedThemeMode,
     );
   }
--- lib/navigation/navigation_container.dart
+++ lib/navigation/navigation_container.dart
@@ -3,78 +3,30 @@
 // found in the LICENSE file.
 
 import 'package:adaptive_theme/adaptive_theme.dart';
-import 'package:flip/constants/navigation.dart';
 import 'package:flip/l10n/language_bloc.dart';
-import 'package:flip/screens/home/home_screen.dart';
-import 'package:flip/screens/login/login_screen.dart';
-import 'package:flip/screens/otp/otp_screen.dart';
-import 'package:flip/screens/splash/splash_screen.dart';
+import 'package:flip/navigation/router.dart';
 import 'package:flip/themes/colors/material_custom_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../l10n/language_state.dart';
 
-GoRoute generateRoute({
-  required String name,
-  Widget? child,
-  GoRouterWidgetBuilder? builder,
-}) {
-  return GoRoute(
-    path: name,
-    name: name,
-    builder: builder ??
-        (BuildContext context, GoRouterState state) {
-          if (child != null) {
-            return child;
-          }
-          return Container();
-        },
-  );
-}
-
-/// The route configuration.
-final GoRouter _router = GoRouter(
-  initialLocation: NavigationRouteName.splash,
-  routes: <RouteBase>[
-    generateRoute(
-      name: NavigationRouteName.splash,
-      child: const SplashScreen(),
-    ),
-    generateRoute(
-      name: NavigationRouteName.home,
-      child: const HomeScreen(),
-    ),
-    generateRoute(
-      name: NavigationRouteName.login,
-      child: const LoginScreen(),
-    ),
-    generateRoute(
-        name: NavigationRouteName.otp,
-        builder: (BuildContext context, GoRouterState state) {
-          final phoneNumber = state.queryParameters['phoneNumber']!;
-          return OtpScreen(phoneNumber: phoneNumber);
-        }),
-  ],
-);
-
-class NavigationContainer extends StatefulWidget {
+class ApplicationContainer extends StatefulWidget {
   final AdaptiveThemeMode? savedThemeMode;
 
-  const NavigationContainer({
+  const ApplicationContainer({
     super.key,
     this.savedThemeMode,
   });
 
   @override
-  State<NavigationContainer> createState() => _NavigationContainerState();
+  State<ApplicationContainer> createState() => _ApplicationContainerState();
 }
 
-class _NavigationContainerState extends State<NavigationContainer> {
+class _ApplicationContainerState extends State<ApplicationContainer> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -92,7 +44,7 @@ class _NavigationContainerState extends State<NavigationContainer> {
               ),
               initial: widget.savedThemeMode ?? AdaptiveThemeMode.light,
               builder: (theme, darkTheme) => MaterialApp.router(
-                    routerConfig: _router,
+                routerConfig: NavigationRouter().router,
                     theme: _buildTheme(theme),
                     debugShowCheckedModeBanner: false,
                     darkTheme: darkTheme,
