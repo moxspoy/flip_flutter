fe7f3fb7506db1c1cdb1a41bfb24c725ae03c113
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-07-29 10:49:08 +0700

    create headline text widget

---
+++ lib/widgets/text/text.dart
@@ -0,0 +1,13 @@
+import 'package:flip/themes/text/custom_text_style.dart';
+import 'package:flutter/material.dart';
+
+class HeadlineText extends StatelessWidget {
+  const HeadlineText(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text, style: CustomTextStyle.headlineLarge(context));
+  }
+}
