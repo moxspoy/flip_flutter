cfc3e7ccacf960e7fe1307e3e72bc9561aca2346
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-08-24 20:44:04 +0700

    handle user bloc of UserUpdatePIN and UserUpdateName

--- lib/blocs/user/user_bloc.dart
+++ lib/blocs/user/user_bloc.dart
@@ -10,18 +10,30 @@ class UserBloc extends HydratedBloc<UserEvent, UserState> {
     on<UserUpdatePhoneNumber>((event, emit) {
       emit(UserDataState(phoneNumber: event.phoneNumber));
     });
+    on<UserUpdatePIN>((event, emit) {
+      emit(UserDataState(pin: event.pin));
+    });
+    on<UserUpdateName>((event, emit) {
+      emit(UserDataState(name: event.name));
+    });
   }
 
   @override
   UserState? fromJson(Map<String, dynamic> json) {
     String phoneNumber = json['phoneNumber'] as String;
-    return UserDataState(phoneNumber: phoneNumber);
+    String name = json['name'] as String;
+    int pin = json['pin'] as int;
+    return UserDataState(phoneNumber: phoneNumber, pin: pin, name: name);
   }
 
   @override
   Map<String, dynamic>? toJson(UserState state) {
     if (state is UserDataState) {
-      return {'phoneNumber': state.phoneNumber};
+      return {
+        'phoneNumber': state.phoneNumber,
+        'pin': state.pin,
+        'name': state.name
+      };
     }
     return {'phoneNumber': '0'};
   }
