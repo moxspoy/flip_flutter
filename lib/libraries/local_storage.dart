a6b46b5a7c8475e1baacd34306ce2f2847c869cb
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2023-09-04 22:34:36 +0700

    add function set and get boolean (shared prefs)

--- lib/libraries/local_storage.dart
+++ lib/libraries/local_storage.dart
@@ -3,10 +3,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 abstract class LocalStorage {
   Future<String?> getString(String key);
 
-  // bool getBoolean();
-
   Future<bool> setString(String key, String value);
-// void setBoolean();
+
+  Future<bool?> getBoolean(String key);
+
+  Future<bool> setBoolean(String key, bool value);
 }
 
 class SharedPrefs implements LocalStorage {
@@ -20,5 +21,17 @@ class SharedPrefs implements LocalStorage {
   Future<bool> setString(String key, String value) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     return await prefs.setString(key, value);
+  }
+
+  @override
+  Future<bool?> getBoolean(String key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(key);
+  }
+
+  @override
+  Future<bool> setBoolean(String key, bool value) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.setBool(key, value);
   }
 }
